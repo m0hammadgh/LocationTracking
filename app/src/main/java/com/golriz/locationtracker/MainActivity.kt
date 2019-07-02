25634f0ff913ae604387a726c42b85531b273484
mohammad <mohammad.ghonchegi@gmail.com>
2019-07-02 14:49:53 +0430

    Distance Calculation Sample imported

--- app/src/main/java/com/golriz/locationtracker/MainActivity.kt
+++ app/src/main/java/com/golriz/locationtracker/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.golriz.locationtracker
 
+import android.location.Location
 import android.os.Bundle
+import android.widget.Toast
 import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import com.golriz.gpstracker.Core.CalculateLocationDistance
 import com.golriz.gpstracker.Core.LocationTracker
 import com.golriz.gpstracker.Core.SettingsLocationTracker.PERMISSION_ACCESS_LOCATION_CODE
 import com.golriz.gpstracker.DB.model.UserCurrentLocation
@@ -18,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         init()
+        Toast.makeText(this, "${calcutePoints()}", Toast.LENGTH_LONG).show()
         btnStop.setOnClickListener {
 
 
@@ -60,6 +64,18 @@ class MainActivity : AppCompatActivity() {
 
                 }
             })
+    }
+
+
+    private fun calcutePoints(): Float {
+        var location1: Location = Location("Location 1")
+        var location2: Location = Location("Location 2")
+        location1.latitude = 36.342247
+        location1.longitude = 59.555205
+
+        location2.latitude = 36.342126
+        location2.longitude = 59.555763
+        return CalculateLocationDistance(location1, location2).calculateDistance()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, @NonNull permissions: Array<String>, @NonNull grantResults: IntArray) {
